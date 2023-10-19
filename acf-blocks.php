2d409191c8523ffd19a07cb4ad8a3fd707cd94a3
WEBzaytsev <studio@webzaytsev.ru>
2023-10-19 21:01:37 +0300

    feat: register blocks

--- acf-blocks.php
+++ acf-blocks.php
@@ -1,1 +1,7 @@
 <?php
+function register_acf_blocks() {
+    register_block_type( __DIR__ . '/blocks/front-page/front-page-first-section' );
+    register_block_type( __DIR__ . '/blocks/front-page/front-page-fourth-section' );
+    register_block_type( __DIR__ . '/blocks/front-page/front-page-second-section' );
+    register_block_type( __DIR__ . '/blocks/front-page/front-page-third-section' );
+}
