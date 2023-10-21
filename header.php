d9c5e829547486175730b90304ef81d3c6394a05
WEBzaytsev <studio@webzaytsev.ru>
2023-10-21 23:28:43 +0300

    refactor: setup header

--- header.php
+++ header.php
@@ -20,40 +20,43 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class( 'text-grey bg-beige overflow-x-hidden' ); ?>>
+<body <?php body_class( 'text-grey bg-beige overflow-x-hidden pt-8' ); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'djun' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$djun_description = get_bloginfo( 'description', 'display' );
-			if ( $djun_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $djun_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<header class="max-w-huge mx-auto">
+		<div class="flex items-center w-full justify-between">
+			<div class="flex items-center gap-x-16.5">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img src="<?php echo esc_url( get_field( 'logotip_dlya_shapki', 'option' )['url'] ); ?>"
+						 alt="img">
+				</a>
+				<nav class="main-navigation">
+					<?php
+					wp_nav_menu(
+						[
+							'theme_location' => 'menu-1',
+							'menu_id' => 'primary-menu',
+						]
+					);
+					?>
+				</nav>
+			</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'djun' ); ?></button>
-			<?php
-			wp_nav_menu(
-				[
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				]
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			<div class="relative">
+				<svg width="376" height="395"
+					 class="absolute z-10 -bottom-[68px] block top-auto -left-[68px]"
+					 viewBox="0 0 376 395" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M351.089 330.563C318.98 377.118 250.408 391.104 197.258 393.774C141.004 396.599 80.568 387.424 41.9418 339.37C2.03951 289.728 -6.57053 218.832 5.39306 155.079C16.9208 93.6482 52.3621 41.9902 102.636 15.9315C150.996 -9.13498 207.115 -1.48754 256.746 24.7947C306.036 50.8961 347.718 95.0412 362.824 153.714C378.184 213.38 383.95 282.916 351.089 330.563Z"
+						  fill="#FEE6A6"/>
+				</svg>
+				<a href="tel:<?php echo esc_attr( get_field( 'telefon', 'option' ) ); ?>"
+				   class="block mb-2 font-unbounded hover:underline text-black text-heading-4-pc font-bold relative z-20">
+					<?php echo esc_html( get_field( 'telefon', 'option' ) ); ?>
+				</a>
+				<p class="text-black font-unbounded text-sm relative z-20">
+					<?php echo esc_html( get_field( 'rezhim_raboty', 'option' ) ); ?>
+				</p>
+			</div>
+		</div>
+	</header>
