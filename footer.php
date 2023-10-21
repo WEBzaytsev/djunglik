23612bf939256a5754a1891b7d01412f5391806e
WEBzaytsev <studio@webzaytsev.ru>
2023-10-21 20:21:57 +0300

    refactor: clear footer

--- footer.php
+++ footer.php
@@ -11,22 +11,8 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'djun' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'djun' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'djun' ), 'djun', '<a href="https://github.com/WEBzaytsev">WEBZaytsev</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<footer class=""></footer>
+</div>
 
 <?php wp_footer(); ?>
 
