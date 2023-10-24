c4fe170ea98e9326f18508053a565b8c78b80d8b
WEBzaytsev <studio@webzaytsev.ru>
2023-10-24 21:01:52 +0300

    refactor(Front Page Second Section Block): change pure text for button to ACF url field

--- blocks/front-page/front-page-second-section/index.php
+++ blocks/front-page/front-page-second-section/index.php
@@ -54,15 +54,19 @@ do_action( 'djun_custom_block_init', $block, $djun_block_slug, $djun_classes );
 				<p class="md:text-pure-text-pc text-pure-text-base md:mb-16 mb-12">
 					<?php the_field( 'tekst' ); ?>
 				</p>
-				<a href="#"
-				   class="bg-ochre rounded-60 px-16 pt-4 pb-5 font-extrabold text-pure-text-pc flex items-center justify-center gap-6 w-fit">
-					<span><?php the_field( 'tekst_knopki' ); ?></span>
-					<svg width="27" height="15" viewBox="0 0 27 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path id="Line 1"
-							  d="M26.7071 8.20711C27.0976 7.81658 27.0976 7.18342 26.7071 6.79289L20.3431 0.428932C19.9526 0.0384078 19.3195 0.0384078 18.9289 0.428932C18.5384 0.819457 18.5384 1.45262 18.9289 1.84315L24.5858 7.5L18.9289 13.1569C18.5384 13.5474 18.5384 14.1805 18.9289 14.5711C19.3195 14.9616 19.9526 14.9616 20.3431 14.5711L26.7071 8.20711ZM0 8.5H26V6.5H0V8.5Z"
-							  fill="#333333"/>
-					</svg>
-				</a>
+				<?php $djun_knopka = get_field( 'knopka' ); ?>
+				<?php if ( $djun_knopka ) : ?>
+					<a href="<?php echo esc_url( $djun_knopka['url'] ); ?>"
+					   class="bg-ochre rounded-60 px-16 pt-4 pb-5 font-extrabold text-pure-text-pc flex items-center justify-center gap-6 w-fit"
+					   target="<?php echo esc_attr( $djun_knopka['target'] ); ?>">
+						<span><?php echo esc_html( $djun_knopka['title'] ); ?></span>
+						<svg width="27" height="15" viewBox="0 0 27 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path id="Line 1"
+								  d="M26.7071 8.20711C27.0976 7.81658 27.0976 7.18342 26.7071 6.79289L20.3431 0.428932C19.9526 0.0384078 19.3195 0.0384078 18.9289 0.428932C18.5384 0.819457 18.5384 1.45262 18.9289 1.84315L24.5858 7.5L18.9289 13.1569C18.5384 13.5474 18.5384 14.1805 18.9289 14.5711C19.3195 14.9616 19.9526 14.9616 20.3431 14.5711L26.7071 8.20711ZM0 8.5H26V6.5H0V8.5Z"
+								  fill="#333333"/>
+						</svg>
+					</a>
+				<?php endif; ?>
 			</div>
 
 			<?php $djun_kartinka = get_field( 'kartinka' ); ?>
