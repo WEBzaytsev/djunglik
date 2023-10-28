ea190801c02203f765254d28e22371d97920d155
WEBzaytsev <studio@webzaytsev.ru>
2023-10-28 21:35:14 +0300

    refactor: remove test block

--- acf-blocks.php
+++ acf-blocks.php
@@ -6,5 +6,4 @@ function register_acf_blocks() {
     register_block_type( __DIR__ . '/blocks/front-page/front-page-third-section' );
     register_block_type( __DIR__ . '/blocks/front-page/news' );
     register_block_type( __DIR__ . '/blocks/front-page/reviews' );
-    register_block_type( __DIR__ . '/blocks/test/test' );
 }
