0e27e9e1b816bb858e679d4f3144bcbfa4455cc8
WEBzaytsev <studio@webzaytsev.ru>
2023-10-30 08:53:06 +0200

    refactor: fix 3d tour width

--- pages/3d-tour.php
+++ pages/3d-tour.php
@@ -12,16 +12,18 @@ get_header();
 		<?php djun_breadcrumbs(); ?>
 
 		<div class="relative z-10 px-5 md:mt-21 mt-4 md:mb-[190px] mb-[235px]">
-			<iframe src="<?php echo esc_url( site_url( '/3dtour/index.htm?media-index=1' ) ); ?>"
-					name="Виртуальный тур"
-					width="100%" height="600"
-					frameborder="0"
-					allow="fullscreen; accelerometer; gyroscope; magnetometer; vr; xr; xr-spatial-tracking; autoplay; camera; microphone"
-					allowfullscreen="true"
-					webkitallowfullscreen="true"
-					mozallowfullscreen="true"
-					oallowfullscreen="true"
-					msallowfullscreen="true"></iframe>
+			<div class="max-w-huge mx-auto">
+				<iframe src="<?php echo esc_url( site_url( '/3dtour/index.htm?media-index=1' ) ); ?>"
+						name="Виртуальный тур"
+						width="100%" height="600"
+						frameborder="0"
+						allow="fullscreen; accelerometer; gyroscope; magnetometer; vr; xr; xr-spatial-tracking; autoplay; camera; microphone"
+						allowfullscreen="true"
+						webkitallowfullscreen="true"
+						mozallowfullscreen="true"
+						oallowfullscreen="true"
+						msallowfullscreen="true"></iframe>
+			</div>
 		</div>
 	</main>
 
